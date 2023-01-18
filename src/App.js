642d94acf8720de609f0d6629816581bef738f46
Merkraith <mark.heil1122@gmail.com>
2023-01-17 20:41:58 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Merkraith's Dragon City Blog</h1>
+      <h1>Dragon City Blog</h1>
       <div id="page-body">
         Welcome to Merkraith's Dragon City Blog!
       </div>
